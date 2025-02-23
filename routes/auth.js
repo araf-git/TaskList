3efa178482469178e6d4654ceb5212b0e4fd5775
araf-git <arafatislam722@gmail.com>
2025-02-23 13:32:55 +0600

    Route Fix

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ import {
   passwordReset,
   refresh,
   logout,
+  verifyEmail
 } from "../controller/auth.js";
 import checkUserAuth from "../middleware/auth.js";
 
@@ -17,6 +18,7 @@ const router = express.Router();
 router
   // Public route
   .post("/signup", limiter, signUp)
+  .post("/verify-email", verifyEmail)
   .post("/login", limiter, login)
   .post("/forgot-password", limiter, sendPasswordResetEmail)
   .post("/reset-password/:id/:reset_token", limiter, passwordReset)
